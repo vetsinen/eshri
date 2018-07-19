27d506d86159176799a251588b9e0ec60fadcb14
Vitaliy Godzevich <v.godzevich@interkassa.com>
2018-07-19 15:45:56 +0300

    edits

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
 <?php
 //info from https://ru.stackoverflow.com/questions/123798/%D0%9F%D0%B5%D1%80%D0%B5%D0%BD%D0%B0%D0%BF%D1%80%D0%B0%D0%B2%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BD%D0%B0-index-php
 echo 'you wanted '.$_SERVER['QUERY_STRING'];
+?>
+<form>
+    <textarea>some text</textarea><br>
+    <input type="submit">
+</form>
+</body>
+</html>
