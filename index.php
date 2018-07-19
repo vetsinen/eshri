a515184f14e715a9b2f0262e5e580871d7324de8
Vitaliy Godzevich <v.godzevich@interkassa.com>
2018-07-19 15:35:19 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+//info from https://ru.stackoverflow.com/questions/123798/%D0%9F%D0%B5%D1%80%D0%B5%D0%BD%D0%B0%D0%BF%D1%80%D0%B0%D0%B2%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BD%D0%B0-index-php
+echo 'you wanted '.$_SERVER['QUERY_STRING'];
